f5a8f89bab495ac70e6f49278452e8eb561601a7
Jerry Guerrero <JerryGDev126@gmail.com>
2025-03-10 19:40:46 -0700

    added in all prices

--- src/components/pages/extensions.js
+++ src/components/pages/extensions.js
@@ -1,23 +1,79 @@
-import Pricing from "../pricing";
+import {
+  Card,
+  CardBody,
+  Divider,
+  Heading,
+  List,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
 
 function extensions() {
   return (
     <div>
-      <Pricing
-        title="Extensions Pricing"
-        list={[
-          "Manicure (with included 5 minute massage) - $20",
-          "Deluxe Manicure (with included 10 minute massage & scrub) - $30",
-          "Gel Manicure - $30",
-          "Dip Manicure - $30",
-          "Gel X - $55",
-          "Removal with service - $5-$10",
-          "Pedicure (with included 5 minute massage) - $25",
-          "Deluxe Pedicure (with included 10 minute massage & scrub) - $35",
-          "Gel Pedicure - $35",
-          "Dip Pedicure - $35",
-        ]}
-      />
+      <Card
+        direction={["column", "column", "row"]}
+        boxShadow="0.5rem 0.5rem 0rem brown"
+        bg="snow"
+        marginTop="1rem"
+        borderRadius="0"
+      >
+        <CardBody>
+          <Heading textColor="coral" textAlign="center">
+            Extension Pricing
+          </Heading>
+          <Divider />
+          <Heading fontSize="2rem" textAlign="center">
+            Tape-in Extensions
+          </Heading>
+          <Text textAlign="center">All Prices Included Installation</Text>
+          <List align="center">
+            <ListItem>14" - 10 Pack - $150</ListItem>
+            <ListItem>18" - 10 Pack - $175</ListItem>
+            <ListItem>22" - 10 Pack - $220</ListItem>
+          </List>
+          <Heading fontSize="2rem" textAlign="center">
+            Fusion Extensions
+          </Heading>
+          <Text textAlign="center">All Prices Included Installation</Text>
+          <List align="center">
+            <ListItem>18" - 20 Pack - $120</ListItem>
+            <ListItem>22" - 20 Pack - $135</ListItem>
+          </List>
+          <Heading fontSize="2rem" textAlign="center">
+            I-tip Extensions
+          </Heading>
+          <Text textAlign="center">All Prices Included Installation</Text>
+          <List align="center">
+            <ListItem>18" - 20 Pack - $120</ListItem>
+            <ListItem>22" - 20 Pack - $135</ListItem>
+          </List>
+          <Heading fontSize="2rem" textAlign="center">
+            Weft Extensions
+          </Heading>
+          <Text textAlign="center">All Prices Included Installation</Text>
+          <List align="center">
+            <ListItem>18.5" - $500</ListItem>
+            <ListItem>22.5"0 - $600</ListItem>
+          </List>
+          <Heading fontSize="2rem" textAlign="center">
+            Clip-in Extensions
+          </Heading>
+          <Text textAlign="center">All Prices Included Installation</Text>
+          <List align="center">
+            <ListItem>16" - $209</ListItem>
+            <ListItem>18" - $219</ListItem>
+            <ListItem>20" - $239</ListItem>
+          </List>
+          <Heading fontSize="2rem" textAlign="center">
+            Misc. Services
+          </Heading>
+          <List align="center">
+            <ListItem>Removal/Re-installation - $250-$400</ListItem>
+            <ListItem>Adjustment Only - $175-$200</ListItem>
+          </List>
+        </CardBody>
+      </Card>
     </div>
   );
 }
